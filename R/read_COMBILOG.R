78fe528f348ad4a3f2933b339eea8aa337ac0bcb
Jose Delgado <jmigueldelgado@gmail.com>
2017-08-24 11:52:46 +0200

    conversion into datetime using lubridate

--- R/read_COMBILOG.R
+++ R/read_COMBILOG.R
@@ -8,7 +8,7 @@ read_COMBILOG <- function(path)
     line <- readLines(path,n=6)[6]
     header <- unlist(strsplit(line,split="\t"))    
     tbl <- read_tsv(path,skip=8,col_names=header) %>%
-        select(-Name) %>%
-        rename(datetime=TimeDate)
+        mutate(datetime=mdy_hms(TimeDate)) %>%
+        select(-Name,-TimeDate)
     return(tbl)
 }
